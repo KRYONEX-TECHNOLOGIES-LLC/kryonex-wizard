003cb524b7a823309293d8d40150a557b4db70cf
DarkNova <darknova046@gmail.com>
2026-01-24 23:53:36 -0500

    Run critical path only in CI

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: process.env.FRONTEND_BASE_URL || "http://localhost:5173",
-    specPattern: "cypress/e2e/**/*.spec.{js,ts}",
+    specPattern:
+      process.env.CYPRESS_SPEC_PATTERN || "cypress/e2e/**/*.spec.{js,ts}",
     supportFile: false,
   },
 });
